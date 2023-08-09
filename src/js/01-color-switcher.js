5eb70ef446450025aebb207915afad30b241937d
Lana2408 <119637611+Lana2408@users.noreply.github.com>
2023-08-09 20:31:30 +0100

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
 
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+
+startButton.addEventListener('click', onStartClick);
+stopButton.addEventListener('click', onStopClick);
+stopButton.disabled = true;
+let interval = null;
+
+function onStartClick(evt) {
+    startButton.disabled = true;
+    stopButton.disabled = false;
+    interval = setInterval(() => {
+        document.body.style.background = getRandomHexColor();}, 1000); 
+
+   }
+   function onStopClick(evt) {
+    startButton.disabled = false;
+    stopButton.disabled = true;
+    clearInterval(interval);
+   }
