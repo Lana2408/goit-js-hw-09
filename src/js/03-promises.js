91965e87ca06f90219ab7a1cd3df1f56bfd18959
Lana2408 <119637611+Lana2408@users.noreply.github.com>
2023-08-09 21:54:37 +0100

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
+const form = document.querySelector('.form');
+form.addEventListener('submit', onButtonClick);
+
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+function onButtonClick(evt) {
+  evt.preventDefault();
+  let delay = Number(evt.target.elements.delay.value);
+  const step = Number(evt.target.elements.step.value);
+  const amount = Number(evt.target.elements.amount.value);
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        console.log(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        console.log(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    delay += step;
   }
 }
+
